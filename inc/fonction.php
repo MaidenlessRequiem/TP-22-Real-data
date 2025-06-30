1a50f99ee8c68aa37c5f409d9184a9108f30136b
John Doe <johndoe@example.com>
2025-06-30 15:51:51 +0300

    OK

--- inc/fonction.php
+++ inc/fonction.php
@@ -25,7 +25,7 @@ function getManager($id){
    return 0 ;
 }
 function getDept_emp($dept_no){
-    $request="select last_name,hire_date,first_name,gender,dept_no from employees  join dept_emp on dept_emp.emp_no=employees.emp_no  where dept_emp.dept_no='%s'" ;
+    $request="select last_name,employees.emp_no,hire_date,first_name,gender,dept_no from employees  join dept_emp on dept_emp.emp_no=employees.emp_no  where dept_emp.dept_no='%s'" ;
     $request=sprintf($request ,$dept_no) ;
     $result=mysqli_query(dbconnect(),$request) ;
     if(mysqli_num_rows($result)){
@@ -42,5 +42,13 @@ $request=sprintf($request ,$dept_no) ;
     $result=mysqli_query(dbconnect(),$request) ;
             $val=mysqli_fetch_assoc($result) ;
     return $val ;    
+}
+function getEmp($emp_no){
+ $request="select * from departments where emp_no='%s'" ;
+$request=sprintf($request ,$dept_no) ;
+    $result=mysqli_query(dbconnect(),$request) ;
+            $val=mysqli_fetch_assoc($result) ;
+    return $val ;    
+
 }
 ?>
